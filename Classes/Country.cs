ac8a4406e5932bfa9e5ef290669f8e72a67d8b2c
STEPS0N <stepan.klimoff@mail.ru>
2026-02-26 09:44:19 +0500

    Разработанный класс State и Country. Климов

---
+++ Classes/Country.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VinylRecordsApplication_Klimov.Classes
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public static IEnumerable<Country> AllCountries()
+        {
+            List<Country> countries = new List<Country>();
+            DataTable requestCountries = DBConnection.Connection("SELECT * FROM [dbo].[Country]");
+            foreach (DataRow row in requestCountries.Rows)
+                countries.Add(new Country()
+                {
+                    Id = Convert.ToInt32(row[0]),
+                    Name = Convert.ToString(row[1])
+                });
+            return countries;
+        }
+    }
+}
---
+++ Classes/State.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace VinylRecordsApplication_Klimov.Classes
+{
+    public class State
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SubName { get; set; }
+        public string Description { get; set; }
+
+        public static IEnumerable<State> AllState()
+        {
+            List<State> allState = new List<State>();
+            DataTable requestSates = DBConnection.Connection("SELECT * FROM [dbo].[state]");
+            foreach (DataRow state in requestSates.Rows)
+                allState.Add(new State()
+                {
+                    Id = Convert.ToInt32(state[0]),
+                    Name = Convert.ToString(state[1]),
+                    SubName = Convert.ToString(state[2]),
+                    Description = Convert.ToString(state[3]),
+                });
+            return allState;
+        }
+
+        public void Save(bool Update = false)
+        {
+            if (Update == false)
+            {
+                DBConnection.Connection(
+                    "INSERT INTO [dbo].[State]([Name], [Subname], [Description]) " +
+                    $"VALUES (N'{this.Name}', N'{this.SubName}', N'{this.Description}');");
+                this.Id = AllState().Where(x => x.Name == this.Name &&
+                                                x.SubName == this.SubName &&
+                                                x.Description == this.Description).First().Id;
+            }
+            else
+            {
+                DBConnection.Connection(
+                    "UPDATE [dbo].[State] SET " +
+                    $"[Name] = N'{this.Name}', " +
+                    $"[Subname] = N'{this.SubName}', " +
+                    $"[Description] = N'{this.Description}', " +
+                    $"WHERE [Id] = {this.Id};");
+            }
+        }
+
+        public void Delete()
+        {
+            DBConnection.Connection($"DELETE FROM [dbo].[State] WHERE [Id] = {this.Id};");
+        }
+    }
+}
