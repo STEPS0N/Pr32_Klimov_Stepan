f1520c4793251ca99dee316bec92fcfce4ebcfe3
STEPS0N <stepan.klimoff@mail.ru>
2026-03-03 11:35:04 +0500

    Разработанная страница добавления и изменения поставок. Климов

--- Pages/Supply/Add.xaml.cs
+++ Pages/Supply/Add.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using VinylRecordsApplication_Klimov.Classes;
 
 namespace VinylRecordsApplication_Klimov.Pages.Supply
 {
@@ -20,9 +21,97 @@ namespace VinylRecordsApplication_Klimov.Pages.Supply
     /// </summary>
     public partial class Add : Page
     {
-        public Add()
+        IEnumerable<Classes.Manufacturer> AllManufacturers = Classes.Manufacturer.AllManufacturers();
+        IEnumerable<Classes.Record> AllRecords = Classes.Record.AllRecords();
+        Classes.Supply changeSupply;
+
+        public Add(Classes.Supply changeSupply = null)
         {
             InitializeComponent();
+            this.changeSupply = changeSupply;
+
+            foreach (var manufacturer in AllManufacturers)
+            {
+                tbManufacturer.Items.Add(manufacturer.Name);
+            }
+            if (tbManufacturer.Items.Count > 0)
+            {
+                tbManufacturer.SelectedIndex = 0;
+            }
+            foreach (var record in AllRecords)
+            {
+                tbRecord.Items.Add(record.Name);
+            }
+            if (tbRecord.Items.Count > 0)
+            {
+                tbRecord.SelectedIndex = 0;
+            }
+
+            if (changeSupply != null)
+            {
+                tbManufacturer.SelectedIndex = AllManufacturers.ToList().FindIndex(x => x.Id == changeSupply.IdManufacturer);
+                tbRecord.SelectedIndex = AllRecords.ToList().FindIndex(x => x.Id == changeSupply.IdRecord);
+                tbCount.Text = changeSupply.Count.ToString();
+
+                DateTime dt = new DateTime();
+                DateTime.TryParse(changeSupply.DateDelivery, out dt);
+                tbDateDelivery.SelectedDate = dt;
+                addBtn.Content = "Изменить";
+            }
+        }
+
+        private void tbPreviewNumber(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !(Char.IsDigit(e.Text, 0));
+        }
+
+        public string CorrectDate(string Value)
+        {
+            DateTime dt = new DateTime();
+            DateTime.TryParse(Value, out dt);
+            return dt.Year + "-" + dt.Month + "-" + dt.Day;
+        }
+
+        private void AddSupply(object sender, RoutedEventArgs e)
+        {
+            DateTime dt = new DateTime();
+
+            if (DateTime.TryParse(tbDateDelivery.SelectedDate.ToString(), out dt))
+            {
+                if (!String.IsNullOrEmpty(tbCount.Text))
+                {
+                    if (changeSupply == null)
+                    {
+                        Classes.Supply newSupply = new Classes.Supply()
+                        {
+                            IdManufacturer = AllManufacturers.Where(x => x.Name == tbManufacturer.SelectedItem.ToString()).First().Id,
+                            IdRecord = AllRecords.Where(x => x.Name == tbRecord.SelectedItem.ToString()).First().Id,
+                            Count = Convert.ToInt32(tbCount.Text),
+                            DateDelivery = CorrectDate(tbDateDelivery.SelectedDate.ToString())
+                        };
+                        newSupply.Save();
+                        MessageBox.Show($"Поставка №{newSupply.Id} успешно добавлена.", "Уведомление");
+                        MainWindow.mainWindow.OpenPage(new Add(newSupply));
+                    }
+                    else
+                    {
+                        changeSupply.IdManufacturer = AllManufacturers.Where(x => x.Name == tbManufacturer.SelectedItem.ToString()).First().Id;
+                        changeSupply.IdRecord = AllRecords.Where(x => x.Name == tbRecord.SelectedItem.ToString()).First().Id;
+                        changeSupply.Count = Convert.ToInt32(tbCount.Text);
+                        changeSupply.DateDelivery = CorrectDate(tbDateDelivery.SelectedDate.ToString());
+                        changeSupply.Save(true);
+                        MessageBox.Show($"Поставка №{changeSupply.Id} успешно изменена.", "Уведомление");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Пожайлуста, укажите количество поставки.", "Предупреждение");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Пожайлуста, укажите дату поставки.", "Предупреждение");
+            }
         }
     }
 }
